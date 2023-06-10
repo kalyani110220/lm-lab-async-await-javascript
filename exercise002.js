328c9da1a2a2b88d96ee472ff90d7127293fe7bf
kalyani <kalyani.nelluri54@gmail.com>
2023-06-10 13:01:28 +0100

    Examples to fetch data of users implemented

--- exercise002.js
+++ exercise002.js
@@ -10,5 +10,14 @@ const fetchData = async (apiEndpoint) => {
   }
 };
 
-const apiEndpoint = "https://getrit.com/API/Token?ref=apilist.fun";
-fetchData(apiEndpoint);
+// Example 1: Fetching data from the JSONPlaceholder API
+const jsonPlaceholderEndpoint = "https://jsonplaceholder.typicode.com/posts";
+fetchData(jsonPlaceholderEndpoint);
+
+// Example 2: Fetching data from the Chuck Norris API
+const chuckNorrisEndpoint = "https://api.chucknorris.io/jokes/random";
+fetchData(chuckNorrisEndpoint);
+
+// Example 3: Fetching data from the SpaceX API
+const spaceXEndpoint = "https://api.spacexdata.com/v4/launches/latest";
+fetchData(spaceXEndpoint);
