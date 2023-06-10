c05ca7c983b66b36af1e6dc314016dda99fae2db
kalyani <kalyani.nelluri54@gmail.com>
2023-06-10 19:45:38 +0100

    Implemented by feteching data by using  freefun api get method

--- exercise002.js
+++ exercise002.js
@@ -1,23 +1,12 @@
 import fetch from "node-fetch";
 
 const fetchData = async (apiEndpoint) => {
-  try {
-    const response = await fetch(apiEndpoint);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+  
+  const response = await fetch(apiEndpoint);
+  const data = await response.json();
+  console.log(data);
+ } 
+ const apiEndpoint = "https://getrit.com/API/Token?ref=apilist.fun";
+ 
+ fetchData(apiEndpoint);
 
-// Example 1: Fetching data from the JSONPlaceholder API
-const jsonPlaceholderEndpoint = "https://jsonplaceholder.typicode.com/posts";
-fetchData(jsonPlaceholderEndpoint);
-
-// Example 2: Fetching data from the Chuck Norris API
-const chuckNorrisEndpoint = "https://api.chucknorris.io/jokes/random";
-fetchData(chuckNorrisEndpoint);
-
-// Example 3: Fetching data from the SpaceX API
-const spaceXEndpoint = "https://api.spacexdata.com/v4/launches/latest";
-fetchData(spaceXEndpoint);
